383560be175556eb77b2eda3d38a700c692dcd71
steven <slstevenlam@gmail.com>
2016-01-11 13:17:44 -0800

    selection sort

--- algos_data_structures_ex/sorts/bubble_sort.js
+++ algos_data_structures_ex/sorts/bubble_sort.js
@@ -1,15 +1,19 @@
 //bubble sort
 
-//O(n^2)
-//minimum number of iterations =  (n-1)
+var bigo = [5, 4, 3, 2, 1]; //
+var bigomega = [1, 2, 3, 4, 5]; //best case
+
+//4(n-1) ...linear
 
 function bubbleSort(arr){
   var len = arr.length;
   var temp = 0;
   var swapping = true;
   while(swapping){
+    console.log("START of while loop")
     var count = 0;
     for(var i = 0; i < len; i++){
+      console.log("=========small iteration #", i)
       //left value greater than right. swap.
       if(arr[i] > arr[i+1]){
         temp = arr[i];
@@ -18,6 +22,7 @@ function bubbleSort(arr){
         count++;
       }
     }
+    console.log("END of while loop")
     if(count > 0){
       swapped = true;
     }
@@ -28,5 +33,6 @@ function bubbleSort(arr){
   return arr
 }
 
-var result = bubbleSort([1, 2, 4, 3, 5]);
-console.log(result);
+// var result = bubbleSort(bigo);
+// console.log(result);
+
